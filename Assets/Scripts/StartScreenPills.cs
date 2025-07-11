ae0785059dfd9645522849d281faeccbdf2fe047
ttchabs <tchabs334@gmail.com>
2025-07-11 17:47:43 +0200

    starts scene

---
+++ Assets/Scripts/StartScreenPills.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreenPills : MonoBehaviour
+{
+    public ParticleSystem startChewpills;
+    
+
+    public void ChewPills()
+    {
+        startChewpills.Play();
+
+
+    }
+
+
+
+   
+}
